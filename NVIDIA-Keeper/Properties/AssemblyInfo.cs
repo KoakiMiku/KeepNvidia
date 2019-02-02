8c0d96c6d782ea0d3dcc7ddf285bc45e77bd95fe
KoakiMiku <2234975857@qq.com>
2019-02-02 20:22:52 +0800

    just change name

--- NVIDIA-Keeper/Properties/AssemblyInfo.cs
+++ NVIDIA-Keeper/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NVIDIA Keeper")]
 [assembly: AssemblyDescription("Keep NVIDIA graphics card always running")]
 [assembly: AssemblyProduct("NVIDIA Keeper")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyCopyright("Copyright © KoakiMiku 2019")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c7c3f68a-3d43-4c7a-b3ac-a7d1e777fa01")]
